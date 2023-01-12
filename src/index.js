c816f84468dc7a3287588cdf814c64a7d98ce7f3
ziyoweb <ziyodulloyevm1@gmail.com>
2023-01-12 15:48:19 +0500

    create build folder

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Fruits from "./Fruits.jsx";
+// import Fruits from "./Fruits.jsx";
 import "./index.css";
-import Student from "./Student.jsx";
-import State from "./State.jsx";
+// import Student from "./Student.jsx";
+// import State from "./State.jsx";
 import Table from "./Table.jsx";
 
-const fruits = [
-  {
-    id: 1,
-    name: "Apple",
-  },
-  { id: 2, name: "Limon" },
-];
+// const fruits = [
+//   {
+//     id: 1,
+//     name: "Apple",
+//   },
+//   { id: 2, name: "Limon" },
+// ];
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
